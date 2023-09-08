f9ae78db61855a91013a187302b49bde921e0a61
Uel Osano <OSNUEL001@myuct.ac.za>
2023-09-08 13:55:02 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using KalahariCollarV13.Data;
+using KalahariCollarV13.Areas.Identity.Data;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("KalahariCollarV13AuthDbContextConnection") ?? throw new InvalidOperationException("Connection string 'KalahariCollarV13AuthDbContextConnection' not found.");
+
+builder.Services.AddDbContext<KalahariCollarV13AuthDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false).AddEntityFrameworkStores<KalahariCollarV13AuthDbContext>();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireUppercase = false;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();
