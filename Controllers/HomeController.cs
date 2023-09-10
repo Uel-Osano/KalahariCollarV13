aa4da9948783fd74511d6118ef01bfc5e25acae6
Uel Osano <OSNUEL001@myuct.ac.za>
2023-09-10 18:02:45 +0200

    Authentication V1

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,29 @@
-﻿using KalahariCollarV13.Models;
+﻿using KalahariCollarV13.Areas.Identity.Data;
+using KalahariCollarV13.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace KalahariCollarV13.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, UserManager<ApplicationUser> userManager)
         {
             _logger = logger;
+            this._userManager = userManager;
         }
 
         public IActionResult Index()
         {
+            ViewData["UserID"] = _userManager.GetUserId(this.User);
             return View();
+            
         }
 
         public IActionResult Privacy()
