21040286cc116f5a42e5ca0d868d0f34d2013f73
Uel Osano <OSNUEL001@myuct.ac.za>
2023-09-14 20:50:02 +0200

    Commit 1

--- Areas/Identity/Data/ApplicationUser.cs
+++ Areas/Identity/Data/ApplicationUser.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
+using KalahariCollarV13.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
@@ -18,5 +19,7 @@ public class ApplicationUser : IdentityUser
     [PersonalData]
     [Column(TypeName = "nvarchar(100)")]
     public string LastName { get; set; }
+    [PersonalData]
+    public List<Pet> Pets { get; set; }
 }
 
